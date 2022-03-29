ea88f8a817124e0be6f307c9ce90c1ec517ab76c
Neeraj <61942571+Neeraj-sudo@users.noreply.github.com>
2022-03-29 16:06:36 +0530

    Models designed and registered to admin

--- OJ_Project/myApp/admin.py
+++ OJ_Project/myApp/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User, Problem
+from .models import User, Problem, Solutions, TestCases
 
 admin.site.register(User)
 admin.site.register(Problem)
+admin.site.register(Solutions)
+admin.site.register(TestCases)
